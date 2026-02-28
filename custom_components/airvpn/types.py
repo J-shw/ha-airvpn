c83e72d7df502f9a5190637c9e20e4467020a714
J-shw <91017208+J-shw@users.noreply.github.com>
2026-02-28 22:58:45 +0000

    Added device types

--- custom_components/airvpn/types.py
+++ custom_components/airvpn/types.py
@@ -1,4 +1,9 @@
-from typing import TypedDict, List, Optional
+from typing import TypedDict, List, Literal
+
+# Now I don't actually know all the literals yet... But I am hoping to add them to this as I do.
+VPNAttemptMessage = Literal["OK"]
+VPNStatus = Literal["ready"]
+ResponseResult = Literal["ok"]
 
 class AirVPNUser(TypedDict):
     login: str
@@ -82,6 +87,49 @@ class AirVPNSession(TypedDict):
     speed_write: int
     """The upload speed."""
 
+class AirVPNDevice(TypedDict):
+    id: str
+    """The ID of the device."""
+    name: str
+    """The name of the device."""
+    description: str
+    """The description of the device."""
+    version: str
+    """The version of encryption."""
+    renew_first_unix: int
+    """The unix timestamp of when the device will renew."""
+    renew_first_date: str
+    """The date the device will renew."""
+    renew_last_unix: int
+    """The unix timestamp of when the device last renewed."""
+    renew_last_date: str
+    """The date the device last renewed."""
+    renew_counter: int
+    """The number of times the device has renewed?"""
+    wireguard_public_key: str
+    """The public key of the device."""
+    wireguard_ipv4: str
+    """The IPv4 address of the device."""
+    wireguard_ipv6: str
+    """The IPv6 address of the device."""
+    vpn_last_from_unix: int
+    """The unix timestamp of when the device last connected."""
+    vpn_last_from_date: str
+    """The date the device last connected."""
+    vpn_last_to_unix: int
+    """The unix timestamp of when the device last disconnected."""
+    vpn_last_to_date: str
+    """The date the device last disconnected."""
+    vpn_attempt_unix: int
+    """The unix timestamp of when the device last attempted to connect."""
+    vpn_attempt_date: str
+    """The date the device last attempted to connect."""
+    vpn_attempt_message: VPNAttemptMessage
+    """The message of the last attempt."""
+    status: VPNStatus
+    """The status of the device."""
+
+
 class UserInfoResponse(TypedDict):
     user: AirVPNUser
     """The user information."""
@@ -89,3 +137,13 @@ class UserInfoResponse(TypedDict):
     """The list of sessions."""
     connection: dict
     """Not sure what this is... It seems to shown a single connection even though I have multiple?"""
+    result: ResponseResult
+    """The result of the request. `ok` seems to be the good response."""
+
+class DeviceInfoResponse(TypedDict):
+    action: str
+    """The action taken."""
+    devices: List[dict]
+    """The list of devices."""
+    result: ResponseResult
+    """`ok` seems to be the good response."""
