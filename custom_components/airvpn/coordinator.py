8e1b79a3e2d46df44a02d4ef975fc7d4b737a4a9
J-shw <91017208+J-shw@users.noreply.github.com>
2026-03-01 10:22:20 +0000

    Uses new scan intervals

--- custom_components/airvpn/coordinator.py
+++ custom_components/airvpn/coordinator.py
@@ -4,21 +4,24 @@ import logging
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from homeassistant.config_entries import ConfigEntry
 
 from .api import AirVPNApi
 from .types import AirVPNData
+from .const import CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
 
 _LOGGER = logging.getLogger(__name__)
 
 class AirVPNUpdateCoordinator(DataUpdateCoordinator[AirVPNData]):
     """Class to manage fetching AirVPN data."""
 
-    def __init__(self, hass: HomeAssistant, api: AirVPNApi) -> None:
+    def __init__(self, hass: HomeAssistant, api: AirVPNApi, entry: ConfigEntry) -> None:
+        scan_interval = entry.options.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL)
         super().__init__(
             hass,
             _LOGGER,
             name="AirVPN",
-            update_interval=timedelta(seconds=300),
+            update_interval=timedelta(seconds=scan_interval),
         )
         self.api = api
 
