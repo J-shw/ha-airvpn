23fc35894e56422131a06a3aa3fac9fb8a5aa392
J-shw <91017208+J-shw@users.noreply.github.com>
2026-03-01 09:41:25 +0000

    Uses new coordinator

--- custom_components/airvpn/__init__.py
+++ custom_components/airvpn/__init__.py
@@ -1,16 +1,34 @@
 import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+
 from .const import DOMAIN, CONF_API_KEY
+from .api import AirVPNApi
+from .coordinator import AirVPNUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     _LOGGER.info("Setting up AirVPN integration")
 
-    api_key = entry.data["api_key"]
-    hass.data.setdefault(DOMAIN, {})[CONF_API_KEY] = api_key
+    api_key = entry.data[CONF_API_KEY]
+    session = async_get_clientsession(hass)
+    api = AirVPNApi(api_key, session)
+    coordinator = AirVPNUpdateCoordinator(hass, api)
+
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
 
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
