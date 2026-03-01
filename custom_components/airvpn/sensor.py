d87dce0daec09339f56bedde116272663be98086
J-shw <91017208+J-shw@users.noreply.github.com>
2026-03-01 10:22:26 +0000

    Update sensor.py

--- custom_components/airvpn/sensor.py
+++ custom_components/airvpn/sensor.py
@@ -19,12 +19,11 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(
     hass: HomeAssistant, 
     entry: ConfigEntry, 
-    async_add_entities: AddEntitiesCallback
+    async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up AirVPN sensors."""
     coordinator: AirVPNUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
 
-    user = coordinator.data["user"]
     entities = []
 
     # User data
